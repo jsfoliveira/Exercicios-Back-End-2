320779fceacbc54197201660cdb352d4bc021a18
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-08-30 10:06:06 -0300

    estudando skip

---
+++ conteudo/skip.js
@@ -0,0 +1,3 @@
+// O método skip() precisa de um parâmetro numérico que determinará quantos documentos serão "pulados" antes de começar a retornar.
+// O exemplo abaixo na coleção bios pulará os dois primeiros documentos e retornará o cursor a partir daí:
+// db.bios.find().skip(2)
