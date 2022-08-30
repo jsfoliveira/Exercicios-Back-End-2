4df0e3afaab6a684a1771ebe82a64276149f6ed4
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-08-30 09:52:12 -0300

    estudando sobre $qt

---
+++ conteudo/tiposComparacao.js
@@ -0,0 +1,2 @@
+// A operação abaixo usa o operador de comparação $gt( greater than, maior que, >) para retornar os documentos em que o valor do atributo qty que tenha um valor que seja maior do que 4:
+// db.collection.find( { qty: { $gt: 4 } } )
