b97c04eb9c76de0f91852034b178ea3c7acb9929
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-08-30 17:54:48 -0300

    treinando sort

---
+++ estudando/sort.js
@@ -0,0 +1,13 @@
+// sort ordena
+// 1 ordena de forma crescente
+// -1 ordena de forma decrescente
+
+// vai procurar todos os value e name e ordenar de forma decrescente e crescente, respectivamente
+
+// db.example.find({}, { value, name }).sort({ value: -1 }, { name: 1 })
+
+// Ordene alfabeticamente os restaurantes pelo nome (atributo name).
+db.example.find().sort( {"name": 1});
+
+// Ordene os restaurantes de forma decrescente baseado nas avaliações.
+db.example.find().sort( {"rating": -1});
