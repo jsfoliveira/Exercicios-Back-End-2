8f70b22f7e7304494d1fc7cdc5972883e6e7bb1a
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-08-31 10:48:34 -0300

    Adicionado comentário

--- estudando/delete.js
+++ estudando/delete.js
@@ -7,6 +7,9 @@ db.inventory.deleteMany({ status : "A" })
 // Para remover todos os documentos da coleção, basta não passar nenhum parâmetro para o método deleteMany():
 db.inventory.deleteMany({})
 
+// Deletar o banco de dados todo
+db.dropDatabase();
+
 // Remova o primeiro restaurante que possua culinária do tipo Ice Cream, Gelato, Yogurt, Ices.
 db.restaurants.deleteOne({cuisine: ['Ice Cream', 'Gelato', 'Yogurt', 'Ices']})
 
