246ae21068a58468fcc93c433195f0f2c86ab3fa
Juliana Oliveira <jsfoliveira89@gmail.com>
2022-09-02 11:16:36 -0300

    Aprendendo sobre o pull

---
+++ estudando/pull.js
@@ -0,0 +1,12 @@
+// Remove do array items os elementos pens e envelopes
+db.supplies.updateMany(
+  {},
+  {
+    $pull: {
+      items: {
+        name: { $in: ["pens", "envelopes"] },
+      },
+    },
+  },
+);
+
